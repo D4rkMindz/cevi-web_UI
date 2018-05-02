af8a8796d6603599daae457d745df2ab37babc20
Björn <bjoern.pfoster@gmail.co>
2018-05-02 14:49:34 +0200

    Added registration route

--- src/app/modules/public/public-routing.module.ts
+++ src/app/modules/public/public-routing.module.ts
@@ -4,11 +4,13 @@ import { PublicComponent } from './public/public.component';
 import { HelloComponent } from './hello/hello.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const PUBLIC_ROUTES: Routes = [
   {path: 'hello', component: HelloComponent},
   {path: 'login', component: LoginComponent},
   {path: 'logout', component: LogoutComponent},
+  {path: 'registration', component: RegistrationComponent},
 ];
 
 const routes: Routes = [
